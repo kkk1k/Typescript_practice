dee8dec02ad50a793e783d8bb9a58ededdca11a7
kkk1k <googija9818@gmail.com>
2025-01-03 18:33:05 +0900

    section 3 : chapter 5

---
+++ section3/src/chapter5.ts
@@ -0,0 +1,17 @@
+// 타입추론
+let a = 10;
+
+let arr = [1, "asd"];
+
+function func1(message = "hello") {
+  console.log(message);
+  return "fuck";
+}
+
+let d;
+d = 10;
+d.toFixed();
+
+d = "hello";
+d.toUpperCase();
+// d.toFixed(); // 오류
